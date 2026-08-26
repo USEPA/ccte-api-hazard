0a9ed1d965d76101504f3cddcc734d83bc46d175
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-08-26 15:40:34 -0400

    CE-5331- updated api version to 1.2.0

--- src/main/java/gov/epa/ccte/api/hazard/config/OpenApiConfig.java
+++ src/main/java/gov/epa/ccte/api/hazard/config/OpenApiConfig.java
@@ -1,29 +1,23 @@
 package gov.epa.ccte.api.hazard.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Computational Toxicology and Exposure (CTX) APIs - CTX Hazard API",
-                description = "The CTX Hazard API is part of US EPA's Computational Toxicology and Exposure APIs. The CTX Hazard API provides programmtic access to hazard data through a set of endpoints.",
-                contact = @Contact(
-                        name = "",
-                        url = "",
-                        email = ""),
-                version = "2.1.0"
-        ),
-        servers = { @Server(url = "${application.api-url}", description = "${application.api-env}")}
-)
 @SecurityScheme(
         type = SecuritySchemeType.APIKEY,
         name = "api_key",
@@ -32,4 +26,34 @@ import org.springframework.context.annotation.Configuration;
         paramName = "x-api-key"
 )
 public class OpenApiConfig {
+
+    private final String apiUrl;
+    private final String apiEnvironment;
+    private final BuildProperties buildProperties;
+
+    public OpenApiConfig(
+            @Value("${application.api-url}") String apiUrl,
+            @Value("${application.api-env}") String apiEnvironment,
+            ObjectProvider<BuildProperties> buildPropertiesProvider
+    ) {
+        this.apiUrl = apiUrl;
+        this.apiEnvironment = apiEnvironment;
+        this.buildProperties = buildPropertiesProvider.getIfAvailable();
+    }
+
+    @Bean
+    public OpenAPI hazardOpenApi() {
+        String version = this.buildProperties != null ? this.buildProperties.getVersion() : "unknown";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Computational Toxicology and Exposure (CTX) APIs - CTX Hazard API")
+                        .description("The CTX Hazard API is part of US EPA's Computational Toxicology and Exposure APIs. The CTX Hazard API provides programmtic access to hazard data through a set of endpoints.")
+                        .contact(new Contact()
+                                .name("")
+                                .url("")
+                                .email(""))
+                        .version(version))
+                .servers(List.of(new Server().url(this.apiUrl).description(this.apiEnvironment)));
+    }
 }
