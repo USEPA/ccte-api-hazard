e0aa1bc242765e992cacae545204c425e8e821bd
jkuntzel <kuntzelman.james@epa.gov>
2026-08-27 08:28:17 -0500

    * added a unit test to double check the maven version against the open-api version * unpinned lombok version * added value application.version to application.yml that exposes the maven project version

---
+++ src/test/java/gov/epa/ccte/api/hazard/web/rest/OpenApiTests.java
@@ -0,0 +1,37 @@
+package gov.epa.ccte.api.hazard.web.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OpenApiTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value("${application.version}")
+    private String expectedVersion;
+
+    @Test
+    void ensureOpenApiVersionMatchesMavenVersion() {
+        String url = "http://localhost:" + port + "/v3/api-docs";
+        String jsonResponse = restTemplate.getForObject(url, String.class);
+
+        // Extract version from OpenAPI JSON path info.version
+        String apiVersion = JsonPath.read(jsonResponse, "$.info.version");
+
+        assertThat(apiVersion)
+                .isNotNull()
+                .isEqualTo(expectedVersion);
+    }
+}
